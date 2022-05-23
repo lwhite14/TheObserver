29360427a841d372f835bde8e5f421017275815d
Luke White <luke_white@yahoo.com>
2022-05-23 17:05:16 +0100

    Refactor TV jumpscare scripts
    And add a static overlay for after the jump scare.

--- Assets/Scripts/Environment/TVJumpScare.cs
+++ Assets/Scripts/Environment/TVJumpScare.cs
@@ -4,14 +4,51 @@ using UnityEngine;
 
 class TVJumpScare : MonoBehaviour
 {
+    [Header("Jump Scare Objects")]
     public GameObject sound;
     public GameObject overlay;
-    public GameObject[] lensFlareObjs;
-    public float overlayTime = 0.25f;
+
+    [Header("Currently In Scene")]
+    public GameObject lensFlare;
+    public GameObject pointLight;
+    public GameObject playerTrigger;
+
+    [Header("Distance To Appear")]
+    public float appearDistance = 20.0f;
+
+    [Header("TV Screen Material")]
+    public MeshRenderer tvMeshRenderer;
+    public Material staticMaterial;
+
+    float overlayTime = 0.25f;
+    Transform playerTransform;
+    Camera worldCamera;
 
     void Start() 
     {
         overlayTime = sound.GetComponent<AudioSource>().clip.length;
+
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        worldCamera = GameObject.Find("WorldRenderer").GetComponent<Camera>();
+
+        lensFlare.SetActive(false);
+        pointLight.SetActive(false);
+        playerTrigger.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (lensFlare != null && pointLight != null)
+        {
+            lensFlare.transform.LookAt(lensFlare.transform.position + worldCamera.transform.rotation * Vector3.forward, worldCamera.transform.rotation * Vector3.up);
+            float distanceBetween = Vector3.Distance(playerTransform.position, lensFlare.transform.position);
+            if (distanceBetween <= appearDistance)
+            {
+                lensFlare.SetActive(true);
+                pointLight.SetActive(true);
+                playerTrigger.SetActive(true);
+            }
+        }
     }
 
     public void JumpScare(Trigger trigger)
@@ -23,14 +60,17 @@ class TVJumpScare : MonoBehaviour
     public void AfterJumpScare(Trigger trigger)
     {
         // Destroy Objects
-        foreach (GameObject obj in lensFlareObjs) 
-        {
-            Destroy(obj);
-        }
+        Destroy(lensFlare);
+        Destroy(pointLight);
+
+        // Change TV
+        tvMeshRenderer.material = staticMaterial;
+
         // Open Next Area
 
+
         // Destroy Self
-        Destroy(gameObject);
+        Destroy(playerTrigger);
     }
 
     IEnumerator DisplayOverlay(Trigger trigger) 
